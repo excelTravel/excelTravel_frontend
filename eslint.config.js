bf354ed2b734df294a0e16698b9bac1bdfee8c90
BonaneN <rbonane336@gmail.com>
2026-07-14 15:42:01 +0200

    Gitignore + eslint-ignore .vite cache; untrack accidentally-committed dep pre-bundle

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default tseslint.config(
-  { ignores: ['dist', 'src/lib/api/types.ts'] },
+  { ignores: ['dist', '.vite', 'src/lib/api/types.ts'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
